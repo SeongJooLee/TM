07ef7c951c05ae12608dec829a21317dcd0210e3
acourve <acourve@naver.com>
2021-12-09 17:03:04 +0900

    길을 잃었어요

--- TravelMaker/src/main/java/com/hk/tm/board/controller/TravelController.java
+++ TravelMaker/src/main/java/com/hk/tm/board/controller/TravelController.java
@@ -1,41 +1,13 @@
 package com.hk.tm.board.controller;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 
-/**
- * Servlet implementation class TravelController
- */
-@WebServlet("/TravelController")
-public class TravelController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TravelController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+@Controller
+public class TravelController {
+	private static final Logger logger = LoggerFactory.getLogger(TravelController.class);
+	
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+	
 }
--- TravelMaker/src/main/java/com/hk/tm/board/service/TravelService.java
+++ TravelMaker/src/main/java/com/hk/tm/board/service/TravelService.java
@@ -1,5 +1,8 @@
 package com.hk.tm.board.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TravelService {
 
 }
