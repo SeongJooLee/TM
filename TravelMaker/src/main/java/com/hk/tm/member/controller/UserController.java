782e04dcbfd1b4ca273f20c0635f4c684020b39e
h0564 <kms970408@naver.com>
2021-12-09 22:55:46 +0900

    김민수 - 로그인(커밋하는거 까먹엇네 키키)

--- TravelMaker/src/main/java/com/hk/tm/member/controller/UserController.java
+++ TravelMaker/src/main/java/com/hk/tm/member/controller/UserController.java
@@ -3,6 +3,7 @@ package com.hk.tm.member.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +22,6 @@ public class UserController {
 	@RequestMapping(value="/member/login",method=RequestMethod.GET)
 	public String memberLogin(Model model) {
 		System.out.println("로그인기능 실행");
-		
 		
 		return "memberLogin";
 	}
@@ -42,25 +42,22 @@ public class UserController {
         	return "memberRegister";            
         case "seller":
         	return "sellerRegister";  
-    }
+		}
 		
 		return null;
 		// 과연
 	}
 	@RequestMapping(value="/member/mypage",method=RequestMethod.POST)
-	public String memberMyPage(@RequestParam("grade")String grade) {
-		System.out.println("실행");
-		 switch(grade){
+	public String memberMyPage(@ModelAttribute UserVO userVO) {
+		System.out.println(userVO);
+		 switch(userVO.getGrade()){
 	        case "user": 
 	        	return "userMyPage";            
 	        case "seller":
 	        	return "sellerMyPage";
 	        case "admin" :
 	        	return "adminMyPage";
-	       
 	    }
 		return null;
-		
-		
 	}
 }
--- TravelMaker/src/main/webapp/WEB-INF/views/memberLogin.jsp
+++ TravelMaker/src/main/webapp/WEB-INF/views/memberLogin.jsp
@@ -9,27 +9,27 @@
 	
 	function register() {
 		location.href = "registerCheck";
-
 	}
 
 	function find() {
 		alert('내년에 개발예정');
-
 	}
 </script>
 </head>
 <body>
+
 	<form action='mypage' method='post'>
-		<input type="radio" name='grade' value='user'> 
-		<input type="radio" name='grade' value='seller'> 
-		<input type="radio" name='grade' value='admin'><br>
+		<p>로그인 구분 방식을 선택해주세용^^</p>
+		구매자 : <input type="radio" name='grade' value='user'> 
+		판매자 : <input type="radio" name='grade' value='seller'> 
+		관리자 : <input type="radio" name='grade' value='admin'><br>
 		<input type='text' name='id' value='ID'><br> 
 		<input type='password' name='pw' value='PW'><br> 
 		<input type='submit' value='로그인'>
 	</form>
+	
 	<button type='button' onclick='find()'>ID/PW찾기</button>
-
 	<button type='button' onclick='register()'>회원가입</button>
-
+	
 </body>
 </html>
