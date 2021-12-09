5aad5779f2fbdbf1e5cff964bad852226e601115
h0564 <kms970408@naver.com>
2021-12-09 16:10:18 +0900

    김민수 header.jsp 로그인 경로수정 회원가입 컨트롤러 / 뷰 추가

--- TravelMaker/src/main/java/com/hk/tm/member/controller/UserController.java
+++ TravelMaker/src/main/java/com/hk/tm/member/controller/UserController.java
@@ -1,8 +1,28 @@
 package com.hk.tm.member.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.hk.tm.member.service.UserService;
+
+
 
 @Controller
 public class UserController {
-
+	@Autowired
+	UserService userService;
+	
+	
+	@RequestMapping(value="/member/register",method=RequestMethod.GET)
+	public String boardList(Model model) {
+		System.out.println("회원가입기능 실행");
+		
+		
+		return "memberRegister";
+	}
 }
