665f34439d06e05b099e9e8bd61c733da6a48de3
acourve <acourve@naver.com>
2021-12-09 13:31:57 +0900

    김나연 likeyouVO 생성

--- TravelMaker/src/main/java/com/hk/tm/board/likeyou/vo/LikeyouVO.java
+++ TravelMaker/src/main/java/com/hk/tm/board/likeyou/vo/LikeyouVO.java
@@ -1,5 +1,40 @@
 package com.hk.tm.board.likeyou.vo;
 
 public class LikeyouVO {
+	private String likecount;
+	private String id;
+	private int travelNO;
+	private int reviewNO;
+	
+	public String getLikecount() {
+		return likecount;
+	}
+	public void setLikecount(String likecount) {
+		this.likecount = likecount;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getTravelNO() {
+		return travelNO;
+	}
+	public void setTravelNO(int travelNO) {
+		this.travelNO = travelNO;
+	}
+	public int getReviewNO() {
+		return reviewNO;
+	}
+	public void setReviewNO(int reviewNO) {
+		this.reviewNO = reviewNO;
+	}
+	
+	@Override
+	public String toString() {
+		return "LikeyouVO [likecount=" + likecount + ", id=" + id + ", travelNO=" + travelNO + ", reviewNO=" + reviewNO
+				+ "]";
+	}
 
 }
