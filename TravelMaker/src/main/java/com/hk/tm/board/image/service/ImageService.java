febd8a395598f767af2b546a9802e74617ef8d41
HKEDU <HKEDU@DESKTOP-RC2HC3S>
2021-12-09 11:39:39 +0900

    성주 - 이미지.컨트/서비스/다오/vo

---
+++ TravelMaker/src/main/java/com/hk/tm/board/image/service/ImageService.java
@@ -0,0 +1,5 @@
+package com.hk.tm.board.image.service;
+
+public class ImageService {
+
+}
--- TravelMaker/src/main/java/com/hk/tm/member/admin/vo/AdminVO.java
+++ TravelMaker/src/main/java/com/hk/tm/member/admin/vo/AdminVO.java
@@ -2,4 +2,37 @@ package com.hk.tm.member.admin.vo;
 
 public class AdminVO {
 
+	String adminID;
+	String pw;
+	String name;
+	String grade;
+	public String getAdminID() {
+		return adminID;
+	}
+	public void setAdminID(String adminID) {
+		this.adminID = adminID;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	@Override
+	public String toString() {
+		return "AdminVO [adminID=" + adminID + ", pw=" + pw + ", name=" + name + ", grade=" + grade + "]";
+	}
+	
 }
