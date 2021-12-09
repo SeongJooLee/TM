9dd448a2c180f61e6c959321b553bfff14d3d794
kkm <81006992+u-naul@users.noreply.github.com>
2021-12-09 13:33:01 +0900

    이벤트 공지사항 홍보상품 vo 입력

--- TravelMaker/src/main/java/com/hk/tm/board/notice/vo/NoticeVO.java
+++ TravelMaker/src/main/java/com/hk/tm/board/notice/vo/NoticeVO.java
@@ -1,5 +1,64 @@
 package com.hk.tm.board.notice.vo;
 
-public class NoticeVO {
+import java.util.Date;
 
+public class NoticeVO {
+	private int NoticeNO;
+	private String title;
+	private String content;
+	private String imagefileName;
+	private Date writeDate;
+	private String id;
+	private String name;
+	
+	public int getNoticeNO() {
+		return NoticeNO;
+	}
+	public void setNoticeNO(int noticeNO) {
+		NoticeNO = noticeNO;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getImagefileName() {
+		return imagefileName;
+	}
+	public void setImagefileName(String imagefileName) {
+		this.imagefileName = imagefileName;
+	}
+	public Date getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(Date writeDate) {
+		this.writeDate = writeDate;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "NoticeVO [NoticeNO=" + NoticeNO + ", title=" + title + ", content=" + content + ", imagefileName="
+				+ imagefileName + ", writeDate=" + writeDate + ", id=" + id + ", name=" + name + "]";
+	}
+	
+	
 }
+
--- TravelMaker/src/main/java/com/hk/tm/board/promotion/vo/PromotionVO.java
+++ TravelMaker/src/main/java/com/hk/tm/board/promotion/vo/PromotionVO.java
@@ -1,5 +1,72 @@
 package com.hk.tm.board.promotion.vo;
 
+import java.util.Date;
+
 public class PromotionVO {
+	private int NoticeNO;
+	private String title;
+	private String content;
+	private String imagefileName;
+	private Date writeDate;
+	private String id;
+	private String name;
+	private int price;
+	public int getNoticeNO() {
+		return NoticeNO;
+	}
+	public void setNoticeNO(int noticeNO) {
+		NoticeNO = noticeNO;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getImagefileName() {
+		return imagefileName;
+	}
+	public void setImagefileName(String imagefileName) {
+		this.imagefileName = imagefileName;
+	}
+	public Date getWriteDate() {
+		return writeDate;
+	}
+	public void setWriteDate(Date writeDate) {
+		this.writeDate = writeDate;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "PromotionVO [NoticeNO=" + NoticeNO + ", title=" + title + ", content=" + content + ", imagefileName="
+				+ imagefileName + ", writeDate=" + writeDate + ", id=" + id + ", name=" + name + ", price=" + price
+				+ "]";
+	}
+	
+	
 
 }
