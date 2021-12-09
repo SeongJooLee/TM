0174b00f7933df430a325d3e1e39bd45e59f34f5
HKEDU <HKEDU@DESKTOP-RC2HC3S>
2021-12-09 18:01:19 +0900

    성주 12-9 마지막 커밋

--- TravelMaker/src/main/java/com/hk/tm/board/dao/ReviewDAO.java
+++ TravelMaker/src/main/java/com/hk/tm/board/dao/ReviewDAO.java
@@ -3,12 +3,13 @@ package com.hk.tm.board.dao;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.hk.tm.board.vo.ReviewVO;
 @Repository
 public class ReviewDAO {
-	
+	@Autowired
 	SqlSession sql;
 	String ns = "mapper.board.";
 	
