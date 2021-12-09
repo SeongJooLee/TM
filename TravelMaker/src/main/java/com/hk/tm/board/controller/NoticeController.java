bc17106591e267b601b371b9eff663ad3153d14c
kkm <81006992+u-naul@users.noreply.github.com>
2021-12-09 17:01:37 +0900

    noticeController 올 주석으로 봉인/ header 공지사항 등 텍스트 변경 / dao service 스프링 티만냄

--- TravelMaker/src/main/java/com/hk/tm/board/controller/NoticeController.java
+++ TravelMaker/src/main/java/com/hk/tm/board/controller/NoticeController.java
@@ -13,43 +13,36 @@ import com.hk.tm.board.service.NoticeService;
 public class NoticeController {
   
 	
-	@Autowired
-	NoticeService noticeService;
-	
-	
-		@RequestMapping(value = "/board/notice", method = RequestMethod.GET)
-		public String noticeList(Model model) {
-			System.out.println("공지사항 클릭 시 페이지 이동");
-			
-			
-			
-			return "noticeList";
-		}
-		
-		@RequestMapping(value = "/board/notice", method = RequestMethod.GET)
-		public String eventList(Model model) {
-			System.out.println("이벤트 클릭 시 안에서 바꾸기");
-			
-			
-			
-			return "eventList";
-		}
-		
-		@RequestMapping(value = "/board/notice/add", method = RequestMethod.GET)
-		public String noticeAdd(Model model) {
-			System.out.println("글쓰기 클릭 시 페이지 이동");
-			
-			
-			
-			return "noticeAdd";
-		}
-		
-		@RequestMapping(value = "/board/notice/view", method = RequestMethod.GET)
-		public String noSelect(Model model) {
-			System.out.println("게시글 클릭 시 페이지 이동");
-			
-			
-			
-			return "?";
-		}
+	/*
+	 * @Autowired NoticeService noticeService;
+	 * 
+	 * 
+	 * @RequestMapping(value = "/board/notice", method = RequestMethod.GET) public
+	 * String noticeList(Model model) { System.out.println("공지사항 클릭 시 페이지 이동");
+	 * 
+	 * 
+	 * 
+	 * return "noticeList"; }
+	 * 
+	 * @RequestMapping(value = "/board/notice", method = RequestMethod.GET) public
+	 * String eventList(Model model) { System.out.println("이벤트 클릭 시 안에서 바꾸기");
+	 * 
+	 * 
+	 * 
+	 * return "eventList"; }
+	 * 
+	 * @RequestMapping(value = "/board/notice/add", method = RequestMethod.GET)
+	 * public String noticeAdd(Model model) { System.out.println("글쓰기 클릭 시 페이지 이동");
+	 * 
+	 * 
+	 * 
+	 * return "noticeAdd"; }
+	 * 
+	 * @RequestMapping(value = "/board/notice/view", method = RequestMethod.GET)
+	 * public String noSelect(Model model) { System.out.println("게시글 클릭 시 페이지 이동");
+	 * 
+	 * 
+	 * 
+	 * return "?"; }
+	 */
 }
