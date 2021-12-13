365c540a489872ffe36187305e5ea1f11d869886
HKEDU <HKEDU@DESKTOP-RC2HC3S>
2021-12-13 13:38:11 +0900

    tjdwn

--- TravelMaker/src/main/java/com/hk/tm/board/controller/NoticeController.java
+++ TravelMaker/src/main/java/com/hk/tm/board/controller/NoticeController.java
@@ -18,10 +18,11 @@ import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.MultipartRequest;
 
 import com.hk.tm.board.service.NoticeService;
 import com.hk.tm.board.vo.ImageVO;
@@ -30,108 +31,109 @@ import com.hk.tm.board.vo.NoticeVO;
 @Controller
 public class NoticeController {
 
-	 @Autowired
-	 NoticeService noticeService;
-	 
+	@Autowired
+	NoticeService noticeService;
+
 	String REPO = "C:\\files";
-	 
-	 
-	 @RequestMapping(value="/board/notice", method=RequestMethod.GET)
-	 public String noticeList(Model model) {
-	
-		 List<NoticeVO> list = noticeService.allList();
-		 
-		 model.addAttribute("notice",list);
-		 
-		 return "noticeList";
-	 }
-	 
-	 @RequestMapping(value="/board/notice/view", method=RequestMethod.GET)
-	 public String noticeView(Model model,@RequestParam("noticeNO") int noticeNO) {
-		 Map<String,Object> map = noticeService.oneList(noticeNO);
-		 System.out.println(map.get("image").toString());
-		 model.addAttribute("notice",map.get("notice"));
-		 model.addAttribute("image",map.get("image"));
-		 return "noticeView";
-	 }
-	 
-	 @RequestMapping(value="/board/notice/add", method=RequestMethod.GET)
-	 public String noticeAdd(Model model) {
-		 return "noticeAdd";
-	 }
-	 
-	 @RequestMapping(value="/board/notice/addDone", method=RequestMethod.POST)
-	 public String noticeAddDone(HttpServletRequest request, HttpServletResponse response,Model model) throws IOException, ServletException {
-			Map<String,String> map = upload(request,response);
-			NoticeVO noticeVO = new NoticeVO();
-			ImageVO imageVO = new ImageVO();
-			int noticeNO = noticeService.oneMaxList();
-			noticeNO++;
-			noticeVO.setNoticeNO(noticeNO);
-			noticeVO.setTitle(map.get("title"));
-			noticeVO.setContent(map.get("content"));
-			noticeVO.setAdminID(map.get("adminID"));
-			noticeVO.setName(map.get("name"));
 
-			String image1 = map.get("image1");
-			String image2 = map.get("image2");
-			imageVO.setImage1(image1);
-			imageVO.setImage2(image2);
-			System.out.println("내가 찾던 그곳 : "+imageVO.toString());
-			
-			if(image1 != null && image1.length()!=0) {
-				File srcFile = new File(REPO+"\\"+"temp"+"\\"+image1);
-				File destDir = new File(REPO+"\\"+noticeVO.getName()+"\\"+noticeVO.getNoticeNO());
-				destDir.mkdir();
-				FileUtils.moveFileToDirectory(srcFile, destDir, true);
-			}
-		 System.out.println("마지막 출력1");
-		 noticeService.boardAdd(noticeVO,imageVO);
-		 System.out.println("마지막 출력2");
 
-		 return "noticeAddDone";
-	 }
-	 
+	@RequestMapping(value="/board/notice", method=RequestMethod.GET)
+	public String noticeList(Model model) {
+
+		List<NoticeVO> list = noticeService.allList();
+
+		model.addAttribute("notice",list);
+
+		return "noticeList";
+	}
+
+	@RequestMapping(value="/board/notice/view", method=RequestMethod.GET)
+	public String noticeView(Model model,@RequestParam("noticeNO") int noticeNO) {
+		Map<String,Object> map = noticeService.oneList(noticeNO);
+		System.out.println(map.get("image").toString());
+		model.addAttribute("notice",map.get("notice"));
+		model.addAttribute("image",map.get("image"));
+		return "noticeView";
+	}
+
+	@RequestMapping(value="/board/notice/add", method=RequestMethod.GET)
+	public String noticeAdd(Model model) {
+		return "noticeAdd";
+	}
+
+	@RequestMapping(value="/board/notice/addDone", method=RequestMethod.POST)
+	public String noticeAddDone(MultipartHttpServletRequest request, HttpServletResponse response,Model model) throws IOException, ServletException {
+		request.setCharacterEncoding("utf-8");
+		Map<String,String> map = upload(request,response);
+		NoticeVO noticeVO = new NoticeVO();
+		ImageVO imageVO = new ImageVO();
+		int noticeNO = noticeService.oneMaxList();
+		noticeNO++;
+		noticeVO.setNoticeNO(noticeNO);
+		noticeVO.setTitle(map.get("title"));
+		noticeVO.setContent(map.get("content"));
+		noticeVO.setAdminID(map.get("adminID"));
+		noticeVO.setName(map.get("name"));
+
+		String image1 = map.get("image1");
+		String image2 = map.get("image2");
+		imageVO.setImage1(image1);
+		imageVO.setImage2(image2);
+		System.out.println("내가 찾던 그곳 : "+imageVO.toString());
+
+		if(image1 != null && image1.length()!=0) {
+			File srcFile = new File(REPO+"\\"+"temp"+"\\"+image1);
+			File destDir = new File(REPO+"\\"+noticeVO.getName()+"\\"+noticeVO.getNoticeNO());
+			destDir.mkdir();
+			FileUtils.moveFileToDirectory(srcFile, destDir, true);
+		}
+		System.out.println("마지막 출력1");
+		noticeService.boardAdd(noticeVO,imageVO);
+		System.out.println("마지막 출력2");
+
+		return "noticeAddDone";
+	}
+
 	private Map<String,String> upload(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-			Map<String,String>noticeMap = new HashMap<String,String>();
-			String encoding="utf-8";
-			File currentDirPath = new File(REPO);
-			DiskFileItemFactory factory = new DiskFileItemFactory();
-			factory.setRepository(currentDirPath);
-			factory.setSizeThreshold(1024*1024);
-			ServletFileUpload upload = new ServletFileUpload(factory);
-			
-			try {
-				List<FileItem> items = upload.parseRequest(request); //주의
-				System.out.println("items의 길이"+items.size());
-				for (int i = 0; i < items.size(); i++) {
-					FileItem fileItem = (FileItem)items.get(i);
-					if(fileItem.isFormField()) {
-						System.out.println("내가 원하는 부분!!"+fileItem.getFieldName()+"="+fileItem.getString(encoding));
-						
-						noticeMap.put(fileItem.getFieldName(), fileItem.getString(encoding));
-					}else {
-						System.out.println("파라미터 이름 :"+fileItem.getFieldName());
-						System.out.println("파일 이름 :"+fileItem.getName());
-						System.out.println("파일 크기 :"+fileItem.getSize()+"bytes");
-						
-						if(fileItem.getSize()>0) {
-							int idx = fileItem.getName().lastIndexOf("\\");
-							if(idx==-1) {
-								idx = fileItem.getName().lastIndexOf("/");
-							}
-							String fileName = fileItem.getName().substring(idx+1);
-							noticeMap.put(fileItem.getFieldName(),fileName);
-							File uploadFile = new File(currentDirPath+"\\temp\\"+fileName);
-							fileItem.write(uploadFile);
+		Map<String,String>noticeMap = new HashMap<String,String>();
+		String encoding="utf-8";
+		File currentDirPath = new File(REPO);
+		DiskFileItemFactory factory = new DiskFileItemFactory();
+		factory.setRepository(currentDirPath);
+		factory.setSizeThreshold(1024*1024);
+		ServletFileUpload upload = new ServletFileUpload(factory);
+
+		try {
+			List<FileItem> items = upload.parseRequest(request); //주의
+			System.out.println("items의 길이"+items.size());
+			for (int i = 0; i < items.size(); i++) {
+				FileItem fileItem = (FileItem)items.get(i);
+				if(fileItem.isFormField()) {
+					System.out.println("내가 원하는 부분!!"+fileItem.getFieldName()+"="+fileItem.getString(encoding));
+
+					noticeMap.put(fileItem.getFieldName(), fileItem.getString(encoding));
+				}else {
+					System.out.println("파라미터 이름 :"+fileItem.getFieldName());
+					System.out.println("파일 이름 :"+fileItem.getName());
+					System.out.println("파일 크기 :"+fileItem.getSize()+"bytes");
+
+					if(fileItem.getSize()>0) {
+						int idx = fileItem.getName().lastIndexOf("\\");
+						if(idx==-1) {
+							idx = fileItem.getName().lastIndexOf("/");
 						}
+						String fileName = fileItem.getName().substring(idx+1);
+						noticeMap.put(fileItem.getFieldName(),fileName);
+						File uploadFile = new File(currentDirPath+"\\temp\\"+fileName);
+						fileItem.write(uploadFile);
 					}
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
 			}
-			return noticeMap;
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-	 
-	
+		return noticeMap;
+	}
+
+
 }
