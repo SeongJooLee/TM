5919b98740b7eae7b63519f81af7ddbee565d120
HKEDU <HKEDU@DESKTOP-RC2HC3S>
2021-12-13 16:48:33 +0900

    성주 충돌대비

--- TravelMaker/src/main/java/com/hk/tm/board/controller/NoticeController.java
+++ TravelMaker/src/main/java/com/hk/tm/board/controller/NoticeController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.hk.tm.board.service.NoticeService;
 import com.hk.tm.board.vo.ImageVO;
