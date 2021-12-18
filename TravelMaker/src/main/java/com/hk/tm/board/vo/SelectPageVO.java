2a37a19402efcf47d80318c383bd6253ac69fb6b
admin <admin@14.42.39.162>
2021-12-18 20:56:17 +0900

    일단 저도

---
+++ TravelMaker/src/main/java/com/hk/tm/board/vo/SelectPageVO.java
@@ -0,0 +1,27 @@
+package com.hk.tm.board.vo;
+
+public class SelectPageVO {
+	private int startPage;
+	private int endPage;
+	private int nowPage;
+	public int getStartPage() {
+		return startPage;
+	}
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+	public int getEndPage() {
+		return endPage;
+	}
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+	public int getNowPage() {
+		return nowPage;
+	}
+	public void setNowPage(int nowPage) {
+		this.nowPage = nowPage;
+	}
+	
+	
+}
