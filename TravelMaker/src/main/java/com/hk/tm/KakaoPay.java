4dabe3c8704802f713fd20fc4b8def11e46f99ae
admin <admin@14.42.39.162>
2021-12-19 14:43:20 +0900

    살려줘

--- TravelMaker/src/main/java/com/hk/tm/KakaoPay.java
+++ TravelMaker/src/main/java/com/hk/tm/KakaoPay.java
@@ -27,27 +27,32 @@ public class KakaoPay {
  
         // 서버로 요청할 Header
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "KakaoAK " + "b35936c88d90573a13525f1103fdde5f");
+        headers.add("Authorization", "KakaoAK" + " b35936c88d90573a13525f1103fdde5f");
         headers.add("Accept", MediaType.APPLICATION_JSON_UTF8_VALUE);
         headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=UTF-8");
         
         // 서버로 요청할 Body
         MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
         params.add("cid", "TC0ONETIME");
-        params.add("partner_order_id", "1001");
-        params.add("partner_user_id", "gorany");
-        params.add("item_name", "갤럭시S9");
+        params.add("partner_order_id", "관리자");
+        params.add("partner_user_id", "이성주");
+        params.add("item_name", "일단 아무거나 팔거야");
         params.add("quantity", "1");
-        params.add("total_amount", "2100");
-        params.add("tax_free_amount", "100");
+        params.add("total_amount", "8500");
+        params.add("tax_free_amount", "0");
         params.add("approval_url", "http://localhost:8080/kakaoPaySuccess");
         params.add("cancel_url", "http://localhost:8080/kakaoPayCancel");
         params.add("fail_url", "http://localhost:8080/kakaoPaySuccessFail");
  
+        System.out.println("헤드 투스트링 : "+headers.toString());
+        System.out.println("파람 투스트링 : "+params.toString());
+        
          HttpEntity<MultiValueMap<String, String>> body = new HttpEntity<MultiValueMap<String, String>>(params, headers);
  
         try {
-            kakaoPayReadyVO = restTemplate.postForObject(new URI(HOST + "/v1/payment/ready"), body, KakaoPayReadyVO.class);
+            System.out.println("여기 들어왓어? 1");
+        	kakaoPayReadyVO = restTemplate.postForObject(new URI(HOST + "/v1/payment/ready"), body, KakaoPayReadyVO.class);
+        	System.out.println("여기 들어왓어? 2");
             
             
             return kakaoPayReadyVO.getNext_redirect_pc_url();
